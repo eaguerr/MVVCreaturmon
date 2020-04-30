a33bcdef1289d684b4779e93f4d2acf936e67a74
guerr <guerralainez12@gmail.com>
2020-04-30 15:25:16 -0500

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/viewmodel/AllCreaturesViewModel.kt
@@ -0,0 +1,15 @@
+package com.raywenderlich.android.creaturemon.viewmodel
+
+import android.arch.lifecycle.ViewModel
+import com.raywenderlich.android.creaturemon.model.CreatureRepository
+import com.raywenderlich.android.creaturemon.model.room.RoomRepository
+
+class AllCreaturesViewModel(private val repository: CreatureRepository =RoomRepository())
+    : ViewModel() {
+
+    private val allCreaturesLiveData = repository.getAllCreatures()
+
+    fun getAllCreaturesLiveData() = allCreaturesLiveData
+
+    fun clearAllCreatures() = repository.clearAllCreatures()
+}
